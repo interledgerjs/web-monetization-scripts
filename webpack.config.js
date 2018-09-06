5d7d01fc0d6afb613f23fd62b66aa4d2eda07f04
sharafian <ben@sharafian.me>
2018-09-05 17:45:35 -0700

    chore: prod

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
 
   entry: {
     'donate': ['@babel/polyfill', './src/donate.js']
